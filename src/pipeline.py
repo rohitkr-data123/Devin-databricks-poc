26d9879774c947410c0add3d4c7bc9e2b3751c9a
rohitkr-data123 <b319043@iiit-bh.ac.in>
2026-09-22 23:05:30 +0530

    Update pipeline.py

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,1 +1,10 @@
+from pyspark.sql import DataFrame
+from pyspark.sql.functions import col
 
+
+def calculate_revenue(df):
+
+    return df.withColumn(
+        "revenue",
+        col("quantity") * col("unit_price")
+    )
